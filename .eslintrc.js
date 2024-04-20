4ae6d2a6cb32f11273b73bb17f53f622015810f9
michalangelo_cali <michelangelocali@hotmail.com>
2024-04-19 22:01:56 -0300

    chore: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,3 +5,12 @@ module.exports = {
   },
   extends: ['universe/native'],
 };
+
+// module.exports = {
+//   parser: 'babel-eslint',
+//   plugins: ['react', 'react-native'],
+//   env: {
+//     'react-native/react-native': true,
+//   },
+//   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-native/all'],
+// };
